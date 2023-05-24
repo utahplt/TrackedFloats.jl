806b53ae5dda624a8366c264d6d336df11fad6b6
Ashton Wiersdorf <mail@wiersdorf.dev>
2023-05-24 16:41:48 -0600

    Start work on bounding logs

---
+++ test/logger_perf_tests.jl
@@ -0,0 +1,58 @@
+# Run this by entering the Julia REPL and running
+   # include("test/logger_perf_tests.jl")
+
+using FloatTracker: TrackedFloat64, write_out_logs, set_exclude_stacktrace, set_logger
+using FileIO, Profile, FlameGraphs, Plots, ProfileView
+
+function track(loops)
+  acc = []
+
+  for i in 1:loops
+    foo = TrackedFloat64(10.0)
+    bar = TrackedFloat64(5.0)
+    baz = bar - (i % 6)
+    push!(acc, (foo - (i % 18)) * (bar / baz) + (bar / baz))
+  end
+
+  return acc
+end
+
+function no_track(loops)
+  acc = []
+
+  for i in 1:loops
+    foo = 10.0
+    bar = 5.0
+    baz = bar - (i % 6)
+    push!(acc, (foo - (i % 18)) * (bar / baz) + (bar / baz))
+  end
+
+  return acc
+end
+
+loops = 50_000
+
+set_logger(filename="log_perf", buffersize=10_000)
+set_exclude_stacktrace([:prop,:kill])
+
+no_track(1)
+track(1)
+
+println("No tracking:")
+Profile.clear();
+@profview no_track(loops)
+# notrack_g = flamegraph(C=true)
+
+println("Halting for input")
+readline()
+
+# FlameGraphs.save("notrack_perf.jlprof", Profile.retrieve()...)
+# Profile.clear();
+
+println("Tracking:")
+@profview track(loops)
+# track_g = flamegraph(C=true)
+# FlameGraphs.save("track_perf.jlprof", Profile.retrieve()...)
+
+println("Halting for input")
+readline()
